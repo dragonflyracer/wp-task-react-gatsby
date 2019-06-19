abda5e1c2591fc4ee8f198dcffa8abeecf825fd9
Jean-Marie Coen <jeanmariecoen@gmail.com>
2019-06-19 11:39:03 -0400

    ajax_get_all (pour les titres)

--- js/ajax_get_all.js
+++ js/ajax_get_all.js
@@ -1,8 +1,6 @@
 var all_task = document.getElementsByClassName("all_task");
 var liste_taches = document.getElementById("liste_taches");
 
-
-
 for (let i = 0; i < all_task.length; i++) {
 
 
@@ -15,18 +13,19 @@ for (let i = 0; i < all_task.length; i++) {
             if (xhr.readyState == 4) {
                 if (xhr.status == 200)
                 var myObj = JSON.parse(this.responseText);
+                console.log(myObj);
                
                     // CHANGEMENT D'IMAGE POUR LE all_task
 
-                    liste_taches.innerHTML=myObj.name;
-                    //Si le all_task est gris, on le fait maintenant apparaître rouge.
-                    if (titreClass.className === "all_task fas fa-heart") {
-                        titreClass.className = titreClass.className.replace("fa-heart", "fa-heart2");
+                    for (var i = 0; i<myObj.length; i++)
+                    {
+                        liste_taches.innerHTML += '</br>' + myObj[i].post_title;
                     }
-                  
-
-            }
-            
+                    // //Si le all_task est gris, on le fait maintenant apparaître rouge.
+                    // if (titreClass.className === "all_task fas fa-heart") {
+                    //     titreClass.className = titreClass.className.replace("fa-heart", "fa-heart2");
+                    // }
+            }            
         }
 
         //Envoie le titre du film ou de la série dans la page likes.php
