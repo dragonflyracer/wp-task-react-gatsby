6a7323ce1375c31151587bbc934e41a5487154f4
Visiteur <visiteur@cegep-fxg.qc.ca>
2019-06-20 17:07:56 -0400

    Correction mineur

--- js/ajax_create_task.js
+++ js/ajax_create_task.js
@@ -1,9 +1,10 @@
 var liste_taches = document.getElementById("liste_taches");
 document.getElementById("create_task").addEventListener("click", create_task, false);
-
+console.log('avant');
 function create_task(e)
 {
   e.preventDefault();
+  console.log('après');
   var titre_tache = document.getElementById("titre_tache").value;
   var description_tache = document.getElementById("description_tache").value;
 
@@ -32,6 +33,7 @@ function create_task(e)
       };            
   };
 
+  // xhr.open('GET', 'http://localhost/wp-task-jpl/wp-json/taskManager/v0/task', true);
   xhr.send(nouvelle_tache);
   // liste_taches.innerHTML = "";
   setTimeout(mettreAjour, 3000)
