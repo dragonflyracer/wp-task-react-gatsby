23af25556a609c2afb039a114d887343bfbaf40f
Pocalman <pasc.levesque@gmail.com>
2019-06-21 12:11:54 -0400

    commit pas

--- express/app.js
+++ express/app.js
@@ -1,17 +1,20 @@
+
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
-app.get('/pascal', function (req, res) {
-    console.log('work');
-    res.send('Comme il est cool')
-})
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
+
+
 
 
